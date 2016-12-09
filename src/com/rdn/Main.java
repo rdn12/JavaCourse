bb6c92304c362934576c54ae5ba2f1b2519980fa
Ardian <ardian_hermono@hotmail.com>
2016-12-09 16:39:17 +1300

    Correction on the previous commit. Section 4 completed and added the challange scenario

--- src/com/rdn/Main.java
+++ src/com/rdn/Main.java
@@ -38,10 +38,16 @@ public class Main {
             System.out.println("is false");
 
         /*
-        This is the Challange
+        This is the Challange:
+            1. Create a double value of 20
+            2. Create a second double value of 80
+            3. Multiply the first to value by 25
+            4. Use the reminder operator to output the remainder of the result in number 3 by 40
+            5. Create an if statement for the result in number 4 to see  if the result is less or equal than 20.
+            If yes then print out a message that say "Total was over the limit"
          */
-        double firstValue = 20d;
-        double secondValue = 80d;
+        double firstValue = 20;
+        double secondValue = 80;
         double totalValue = (firstValue + secondValue) * 25;
         double remainderValue = totalValue % 40;
 
