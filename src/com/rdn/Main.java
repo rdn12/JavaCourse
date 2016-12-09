7fe9413240a7861d944ca586edcf0a500957479b
Ardian <ardian_hermono@hotmail.com>
2016-12-09 16:25:39 +1300

    Completed section 5 lecture 24

--- src/com/rdn/Main.java
+++ src/com/rdn/Main.java
@@ -15,7 +15,40 @@ public class Main {
         int secondScore = 60;
         if (firstScore > secondScore && secondScore >100)
             System.out.println("shit just got real");
+        if ((firstScore > 40) || (secondScore < 60))
+            System.out.println("I think I got this right?");
         else
             System.out.println("this is just getting out of control");
+
+        int testValue = 50;
+        if (testValue == 51)
+            System.out.println("you got a fiddy");
+        else
+            System.out.println("you broke®");
+
+        boolean isTaco = true;
+        if (isTaco == true)
+            System.out.println("free taco for everyone");
+
+        isTaco = true;
+        boolean wasTaco = isTaco ? true : false;
+        if (wasTaco)
+            System.out.println("is true");
+        else
+            System.out.println("is false");
+
+        /*
+        This is the Challange
+         */
+        double firstValue = 20d;
+        double secondValue = 80d;
+        double totalValue = (firstValue + secondValue) * 25;
+        double remainderValue = totalValue % 40;
+
+        if (remainderValue <= 20)
+            System.out.println("Total was over the limit");
+        else
+            System.out.println("You good!");
+
     }
 }
